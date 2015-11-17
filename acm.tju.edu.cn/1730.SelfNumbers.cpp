8f3c491dc0f6b28ab800fe64edd0e0ea091bb85b
Stefan Negrus <stefan.negrus@gmail.com>
2015-11-17 11:47:13 +0200

    Added first 4 problems from tju contest

---
+++ acm.tju.edu.cn/1730.SelfNumbers.cpp
@@ -0,0 +1,114 @@
+#ifdef ONLINE_JUDGE
+#include <bits/stdc++.h>
+#else
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#endif
+
+using namespace std;
+
+	// lambda : [] (int a, int b) -> bool { body return }
+	// string r_str = R"(raw string)"
+
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define pb push_back
+#define LL long long
+#define ULL unsigned long long
+#define BASE 73
+#define NMAX 10000
+#define NMAX2 20001
+#define MOD1 1000000007
+#define ALL(V) (V).begin(), (V).end()
+#define ALLR(V) (V).rbegin(), (V).rend()
+#define CRLINE Duxar(__LINE__);
+#define SHOWME(x) cerr << __LINE__ << ": " << #x << " = " << (x) << endl;
+#define ENTER putchar('\n');
+
+int dx4[] = {-1, 0, 1, 0};
+int dy4[] = {0, 1, 0, -1};
+
+int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+void Duxar(int _this_line) {
+#ifndef ONLINE_JUDGE
+	printf("\n . . . . . . . . . . . . . Passed line - %d\n", _this_line);
+#endif
+}
+
+template <class T>
+void ReadNo(T &_value) {
+	T _sign = 1;
+	char ch;
+	_value = 0;
+	while(!isdigit(ch = getchar())) {
+		(ch == '-') && (_sign = -1);
+	}
+	do {
+		_value = _value * 10 + (ch - '0');
+	} while(isdigit(ch = getchar()));
+	_value *= _sign;
+}
+
+template <class T>
+void AddNr(T &a, T b) {
+	a = a + b;
+	while (a >= MOD1) {
+		a -= MOD1;
+	}
+	while (a < 0) {
+		a += MOD1;
+	}
+}
+
+int N;
+int get_next(int x) {
+	int aux = x, sum = x;
+	while (aux) {
+		sum += aux % 10;
+		aux /= 10;
+	}
+	return sum;
+}
+
+bitset<10001> res;
+
+int main(){
+		//	string fileInput = "sum";
+		//#ifdef INFOARENA
+		//	freopen((fileInput + ".in").c_str(), "r", stdin);
+		//	freopen((fileInput + ".out").c_str(), "w", stdout);
+		//#else
+#ifndef ONLINE_JUDGE
+	freopen("input", "r", stdin);
+#endif
+		//#endif
+
+	int i, x, y, j;
+
+	for (i = 1; i <= 10000; ++i) {
+		if (res[i] == 0) {
+			printf("%d\n", i);
+			for (j = get_next(i); j <= 10000 && res[j] == 0; j = get_next(j)) {
+				res[j] = 1;
+			}
+		}
+	}
+
+	return 0;
+}
